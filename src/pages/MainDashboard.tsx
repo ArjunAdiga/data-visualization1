4643e135f6ae315b65701d0c5204109d828812ea
Arjun <arjunadiga191200@gmail.com>
2025-06-26 20:54:53 +0530

    declared but not used variables

--- src/pages/MainDashboard.tsx
+++ src/pages/MainDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Typography from "../components/Typography";
 import Button from "../components/Button";
 import Graphs from "../components/Graphs";
